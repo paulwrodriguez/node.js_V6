384378d61cc970ca2c4577f985d3e47557662e6a
Vantiv <douglas.bay@vantiv.com>
2015-03-24 09:06:53 -0700

    Corrected fields
    Correct location and addition of fields "credentials", "EntryMode" and
    "PartialApprovalCode"

--- src/capture.js
+++ src/capture.js
@@ -1,9 +1,23 @@
-//Set all the values here
-var credentials = {
-	AccountID : 's.MID5.PAY.WS.NP',
-	Password : 'Tu2u2AHU'
-}
+/*
+Copyright (c) 2014 Vantiv, Inc. - All Rights Reserved.
 
+Sample Code is for reference only and is solely intended to be used for educational purposes and is provided “AS IS” and “AS AVAILABLE” and without 
+warranty. It is the responsibility of the developer to  develop and write its own code before successfully certifying their solution.  
+
+This sample may not, in whole or in part, be copied, photocopied, reproduced, translated, or reduced to any electronic medium or machine-readable 
+form without prior consent, in writing, from Vantiv, Inc.
+
+Use, duplication or disclosure by the U.S. Government is subject to restrictions set forth in an executed license agreement and in subparagraph (c)(1) 
+of the Commercial Computer Software-Restricted Rights Clause at FAR 52.227-19; subparagraph (c)(1)(ii) of the Rights in Technical Data and Computer 
+Software clause at DFARS 252.227-7013, subparagraph (d) of the Commercial Computer Software--Licensing clause at NASA FAR supplement 16-52.227-86; 
+or their equivalent.
+
+Information in this sample code is subject to change without notice and does not represent a commitment on the part of Vantiv, Inc.  In addition to 
+the foregoing, the Sample Code is subject to the terms and conditions set forth in the Vantiv Terms and Conditions of Use (http://www.apideveloper.vantiv.com) 
+and the Vantiv Privacy Notice (http://www.vantiv.com/Privacy-Notice).  
+*/
+
+//Set all the values here
 var merchant = {
 	NetworkRouting : '2J',
 	CashierNumber : '12345678',
@@ -16,6 +30,7 @@ var merchant = {
 
 var terminal = {
 	TerminalID : '1',
+	EntryMode : 'manual',
 	Ipv4Address : '192.0.2.235',
 	TerminalEnvironmentalCode : 'electronic_cash_register',
 	PinEntry : 'none',
@@ -39,7 +54,8 @@ var transaction = {
 	CaptureAmount : '10.00',
 	OriginalReferenceNumber: '',
 	TokenRequested : 'false',
-	SystemTraceID : '100002'
+	SystemTraceID : '100002',
+	PartialApprovalCode: 'not_supported'
 }
 
 var address = {
@@ -48,7 +64,6 @@ var address = {
 
 var card = {
 	CardType: 'visa',
-	PartialApprovalCode: 'not_supported',
 	CardNumber: '4445222299990007',
 	ExpirationMonth: '12',
 	ExpirationYear:'2017',
@@ -57,7 +72,6 @@ var card = {
 }
 
 var authorize = {
-	credentials : credentials,
 	merchant: merchant,
 	terminal: terminal,
 	transaction: transaction,
--- src/send_request.js
+++ src/send_request.js
@@ -1,10 +1,29 @@
+/*
+Copyright (c) 2014 Vantiv, Inc. - All Rights Reserved.
+
+Sample Code is for reference only and is solely intended to be used for educational purposes and is provided “AS IS” and “AS AVAILABLE” and without 
+warranty. It is the responsibility of the developer to  develop and write its own code before successfully certifying their solution.  
+
+This sample may not, in whole or in part, be copied, photocopied, reproduced, translated, or reduced to any electronic medium or machine-readable 
+form without prior consent, in writing, from Vantiv, Inc.
+
+Use, duplication or disclosure by the U.S. Government is subject to restrictions set forth in an executed license agreement and in subparagraph (c)(1) 
+of the Commercial Computer Software-Restricted Rights Clause at FAR 52.227-19; subparagraph (c)(1)(ii) of the Rights in Technical Data and Computer 
+Software clause at DFARS 252.227-7013, subparagraph (d) of the Commercial Computer Software--Licensing clause at NASA FAR supplement 16-52.227-86; 
+or their equivalent.
+
+Information in this sample code is subject to change without notice and does not represent a commitment on the part of Vantiv, Inc.  In addition to 
+the foregoing, the Sample Code is subject to the terms and conditions set forth in the Vantiv Terms and Conditions of Use (http://www.apideveloper.vantiv.com) 
+and the Vantiv Privacy Notice (http://www.vantiv.com/Privacy-Notice).  
+*/
+
 var auth = require('./auth.js')
 var capture = require('./capture.js')
 var cancel = require('./cancel.js')
 var http = require('http')
 
-//licenseID used in the header
-var licenseID = '572d606c967f412cb8d840e38fb48010$$#$$MphfoMed030iGRXOd6pBhDzGQnzEMmz7$$#$$2015-11-26$$#$$dev_key$$#$$SHA512withRSA$$#$$RSA$$#$$1$$#$$52BC72E18D55CC77AE2DE3C27C0AFE6C0FCE3E3E6C1638EE824E5DB7775ADBBB616D82127D46DF9272D6A39ABC6BA8AD6797000A52F769C982B5360C06CED1FFA8FABA0DEA70E1CD77DC4DAF912E81319538857CAABE16DB6C412AF478BC059B29232337AE09020069B96E741982FB5E6BC053E98FE7C33789288CAA6A9883C0D2380DD1812FCF2681A8B31545B97DD0736EB3ECBD9329F144CDB93C35780559DB6219604ADB3F5A8DC57E06CFD715FFCECD3CB65DE7BFDA065D4DB3BDC84B7E0FD66116C71AC41E0B875BA18C742A8ECE9E690AC37457DD43845F5C64EA00BF7B585A6FCF75A0F398026CD79C74C3C946C11BAFD1404997917C5592B91E8415'
+//licenseID used in the header. You will need to obtain a LicenseId before running this sample. 
+var licenseID = ''
 var header = {
 	'Content-Type': 'application/json',
 	'licenseid': licenseID
